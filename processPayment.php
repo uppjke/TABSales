c6995ee3dd7b12da759d216354c2c494dfd72f5a
Vadim Gusev <43630752+uppjke@users.noreply.github.com>
2023-09-14 17:49:44 +0300

    Update processPayment.php

--- processPayment.php
+++ processPayment.php
@@ -1,1 +1,17 @@
+<?php
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $billId = $_POST['billId'];
+    $email = $_POST['email'];
+
+    // Проверьте статус оплаты с помощью API QIWI
+
+    // Если оплата прошла успешно, отправьте почту
+    $to = $email;
+    $subject = "Подтверждение оплаты";
+    $message = "Спасибо за покупку табулатуры. Ваш счет: " . $billId;
+
+    mail($to, $subject, $message);
+}
+
+?>
