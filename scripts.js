4e05e06c1f2870f327413e5e6be1ac28c20454ec
Vadim Gusev <43630752+uppjke@users.noreply.github.com>
2023-09-14 18:30:23 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-function buy(productName) {
-    const amount = productName === 'Табулатура 1' ? 100 : 150; // Здесь упрощенное условие, в реальности следует использовать другой метод определения цены
-
-    const billId = "UNIQUE_BILL_ID_" + Date.now(); // Уникальный ID счета
-
-    const paymentURL = `https://oplata.qiwi.com/create?publicKey=YOUR_PUBLIC_KEY&amount=${amount}&billId=${billId}&comment=${productName}`;
-
-    // Открываем URL для оплаты
-    window.open(paymentURL, '_blank');
+function showPaymentForm(productName, amount) {
+    const form = document.getElementById('paymentForm');
+    
+    form.querySelector('input[name="productName"]').value = productName;
+    form.querySelector('input[name="amount"]').value = amount;
+    
+    form.style.display = 'block';
 }
